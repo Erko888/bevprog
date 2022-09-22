74b8b9e167099d11f31206b403c9ca6cc2773be1
Erko888 <erikkotai@gmail.com>
2022-09-22 20:14:51 +0200

    Create age.py
    python homework

---
+++ age.py
@@ -0,0 +1,29 @@
+def main():
+    yourAge = int(input("How old are you: "))
+    americanLegalDrinkingAge = 21
+    abletoBuyCigarettesHungary = 18
+    abletoGetDriversLicenseHungary = 17
+    shrek2AgeLimit = 12
+
+    if yourAge>=americanLegalDrinkingAge:
+        print("You can legally drink in the USA")
+    else:
+        print("You are not allowed to legally drink in the USA")
+    
+    if yourAge>=abletoBuyCigarettesHungary:
+        print("You can legally buy cigarettes in Hungary")
+    else:
+        print("You can't legally buy cigarettes in Hungary")
+    
+    if yourAge>=abletoGetDriversLicenseHungary:
+        print("You can go and get your drivers license in Hungary")
+    else:
+        print("You can't get your drivers license in Hungary yet")
+    
+    if yourAge>=shrek2AgeLimit:
+        print("You can watch Shrek 2")
+    else:
+        print("You can't watch Shrek 2 yet")
+    
+if __name__ == "__main__":
+    main()
